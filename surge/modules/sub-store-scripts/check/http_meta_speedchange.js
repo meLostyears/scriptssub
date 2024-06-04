200d470404e3c85679e43645b310f308ab2d25c1
meLostyears <96301795+meLostyears@users.noreply.github.com>
2024-06-04 12:31:16 +0800

    Update http_meta_speedchange.js

--- surge/modules/sub-store-scripts/check/http_meta_speedchange.js
+++ surge/modules/sub-store-scripts/check/http_meta_speedchange.js
@@ -175,7 +175,7 @@ async function operator(proxies = [], targetPlatform, context) {
       const status = parseInt(res.status || res.statusCode || 200)
       let latency = ''
       latency = `${Date.now() - startedAt}`
-      const speed = Math.round((bytes / 1024 / 1024 / (latency / 1000)) * 8).padStart(5, '0') + ' M'
+      const speed = Math.round((bytes / 1024 / 1024 / (latency / 1000)) * 8).padStart(3, '0') + ' M'
       $.info(`[${proxy.name}] status: ${status}, latency: ${latency}, speed: ${speed}`)
       // 判断响应
       if (speed) {
